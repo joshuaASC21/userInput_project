22447b505ed0653fa870d018ae1415302407518d
joshuaASC21 <josh.dlc0309@gmail.com>
2021-08-03 13:23:56 -0400

    2

---
+++ script.js
@@ -0,0 +1,37 @@
+let userName = document.querySelector("#username");
+let password= document.querySelector("#password");
+
+let sucessMessage= document.querySelector("#success");
+
+let body= document.querySelector("body");
+let errorMessage= document.querySelector("#error");
+let submitButton= document.querySelector("#submit-button");
+
+submitButton.onclick= function(event){
+    event.preventDefault();
+
+   
+    let userNameInput= document.createElement("text");
+    userNameInput.innerHTML = userName.value;
+
+    let passwordInput= document.createElement('password')
+    passwordInput.innerHTML= password.value;
+    if (userName.value== "Baby Yoda" && password.value== "Squirtle"){
+        sucessMessage.style.display= "block";
+        body.style.backgroundColor= "rgb(20,230,10)";
+
+
+    }
+     else {
+        errorMessage.style.display= "block";
+        sucessMessage.style.display= "none";
+        body.style.backgroundColor= "firebrick";
+    }
+
+}
+submitButton.ondblclick= function(){
+    body.style.backgroundColor= "white";
+}
+
+
+
